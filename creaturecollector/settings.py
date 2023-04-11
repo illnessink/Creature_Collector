02ed499f26f036a6238410075bdea14f5f3c5c9b
Vanessa Foerster <nessfoerster@gmail.com>
2023-04-11 19:26:55 -0400

    wip: fixes deployment settings

--- creaturecollector/settings.py
+++ creaturecollector/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -129,3 +129,8 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# configure django to work within heroku production environment
+import django_heroku
+django_heroku.settings(locals())
